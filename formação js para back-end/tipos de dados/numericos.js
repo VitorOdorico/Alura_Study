283df7d2acd740fe6d365b5b34b1486603a6d013
VitorOdorico <ve743137@gmail.com>
2022-12-21 00:15:17 -0300

    add vareaveis.js and numericos.js to formação js para back end

---
+++ formação js para back-end/tipos de dados/numericos.js
@@ -0,0 +1,7 @@
+const idade = 30;
+const pi = 3.14
+
+
+console.log(typeof(idade))
+console.log(typeof(pi))
+
---
+++ formação js para back-end/tipos de dados/vareaveis.js
@@ -0,0 +1,11 @@
+let numeroUm = 10
+let numeroDois = 30
+
+const soma = numeroUm + numeroDois;
+
+console.log("A soma dos valores é ", soma)
+
+// resultado:
+
+// [Running] node "c:\projetos dev\alura\formação js para back-end\tipos de dados\vareaveis.js"
+// A soma dos valores é  40
