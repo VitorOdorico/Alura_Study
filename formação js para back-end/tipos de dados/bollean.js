e23d52e2da94365dbf246bf700d7a0c2b5d5dfb3
VitorOdorico <ve743137@gmail.com>
2022-12-21 09:51:29 -0300

    mais um exemplo em bollean.js

--- formação js para back-end/tipos de dados/bollean.js
+++ formação js para back-end/tipos de dados/bollean.js
@@ -1,4 +1,12 @@
 let primeiroNumero = 5;
-let segundoNumero = 6;
+let segundoNumero = 5;
 
 console.log(primeiroNumero === segundoNumero)
+// saida de dados: true
+
+let a = 'a';
+let A = 'A';
+
+console.log(a === A)
+
+// saida de dados: false
