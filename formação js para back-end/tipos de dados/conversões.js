2c4989c098205c896d5b394671e1ae20e18efef6
VitorOdorico <ve743137@gmail.com>
2022-12-22 11:09:02 -0300

    conversoes.js

--- formação js para back-end/tipos de dados/conversões.js
+++ formação js para back-end/tipos de dados/conversões.js
@@ -1,4 +1,18 @@
-const numero = 456;
-const numeroString = '456';
+// tipos de dados Boleanos
+
+// conversã implicita
+let numero = 456;
+let numeroString = '456';
+
+
+// Number()
+// String()
+
 
 console.log(numero === Number(numeroString))
+
+// conversão explicita
+
+numeroString = Number('456')
+
+console.log(numero + numeroString)
