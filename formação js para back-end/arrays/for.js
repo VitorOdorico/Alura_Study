a2d72844671e57449e780687e0b434711a53ee93
VitorOdorico <ve743137@gmail.com>
2023-01-04 16:39:51 -0300

    att for js

---
+++ formação js para back-end/arrays/for.js
@@ -0,0 +1,9 @@
+// let inicial = 100;
+// for(let i = 0; inicial <= 600; inicial+= 100){
+//     console.log(inicial)
+// }
+
+const list = [100, 200, 300, 400, 500, 600]
+for(let i = 0;list <= list.length[i]; i += 1){
+    console.log(list)
+}
